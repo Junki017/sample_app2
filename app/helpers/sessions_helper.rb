a035d30dd56fd10acd6945a7f308a94ae55ad47b
Junki017 <yunqui1201@gmail.com>
2019-09-12 16:49:35 +0000

    a bit change

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,14 +15,13 @@ module SessionsHelper
   end
 
   def current_user
-    if (user_id = session[:user_id])
-      # binding.pry
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
+    if (user_id = session[:user_id])                                            # 一時的なセッションユーザーがいる場合処理を行い、user_idに代入
+      @current_user ||= User.find_by(id: user_id)                               # 現在のユーザーがいればそのまま、いなければsessionユーザーidと同じidを持つユーザーをDBから探して@current_user（現在のログインユーザー）に代入
+    elsif (user_id = cookies.signed[:user_id])                                  # user_idを暗号化した永続的なユーザーがいる（cookiesがある）場合処理を行い、user_idに代入
+      user = User.find_by(id: user_id)                                          # 暗号化したユーザーidと同じユーザーidをもつユーザーをDBから探し、userに代入
+      if user && user.authenticated?(cookies[:remember_token])                  # DBのユーザーがいるかつ、受け取った記憶トークンをハッシュ化した記憶ダイジェストを持つユーザーがいる場合処理を行う
+        log_in user                                                             # 一致したユーザーでログインする
+        @current_user = user                                                    # 現在のユーザーに一致したユーザーを設定
       end
     end
   end
